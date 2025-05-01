f69cb0c49c8df2152adb667ea5fe70ffe54ed3dc
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-01 18:08:03 +0000

    Intital Commit (Spring Initializr)

---
+++ src/main/java/com/vtkr/app/scribeit/ScribeitApplication.java
@@ -0,0 +1,13 @@
+package com.vtkr.app.scribeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribeitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribeitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtkr/app/scribeit/ScribeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtkr.app.scribeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
