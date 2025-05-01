41e16e4badaa316c663163c10c286afb86f94738
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-01 18:46:10 +0000

    Rename Files

--- src/main/java/com/vtkr/app/scribeit/ScribeItApplication.java
+++ src/main/java/com/vtkr/app/scribeit/ScribeItApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScribeitApplication {
+public class ScribeItApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScribeitApplication.class, args);
+		SpringApplication.run(ScribeItApplication.class, args);
 	}
 
 }
